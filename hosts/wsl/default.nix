7d632bfdfd4b35b1a06d0d6bd1a86b3bbb9439d8
Mihkel Mäemees <maemeesmihkel@gmail.com>
2025-07-16 14:05:40 +0000

    Added user info to git

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -21,7 +21,6 @@
 
   environment.systemPackages = with pkgs; [
     wget
-    git
     neovim
     nixfmt-rfc-style
     gh
--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -4,6 +4,14 @@
   home.username = "mike";
   home.homeDirectory = "/home/mike";
 
+  programs = {
+    git = {
+      enable = true;
+      userName = "Mihkel Mäemees";
+      userEmail = "maemeesmihkel@gmail.com";
+    };
+  };
+
   home.stateVersion = "25.05"; # No changing
 
   programs.home-manager.enable = true;
